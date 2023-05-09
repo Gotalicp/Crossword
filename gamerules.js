3070e089b2cbf5f13ddcd09404c7aceea179c88b
Gotalic <gotalicbgyt@gmail.com>
2023-05-09 09:52:28 +0300

    working?

--- gamerules.js
+++ gamerules.js
@@ -1,16 +1,16 @@
 //connects sql to js
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'your_username',
-  password: 'your_password',
-  database: 'englsihdictionary'
-});
+// const mysql = require('mysql');
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'your_username',
+//   password: 'your_password',
+//   database: 'englsihdictionary'
+// });
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to MySQL database');
-});
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log('Connected to MySQL database');
+// });
 
 //makes the grid and word arrays
 const words = new Array();
@@ -18,7 +18,12 @@ const size = 15;
 const grid = new Array(size);
 for (let i = 0; i < size; i++) {
   grid[i] = new Array(size).fill('');
+  for(let j =0 ;j < size ; j++){
+    $("#container").append("<div class='grid'></div>");
+  }
 }
+$(".grid").width(960/size);
+$(".grid").height(960/size);
 
 //places the words in place
 function placeWord(word, x, y, dx, dy) {
@@ -30,14 +35,14 @@ function placeWord(word, x, y, dx, dy) {
 }
 
 //getting and placing the first word in the middle
-connection.query('SELECT word FROM entries ;WHERE CHAR_LENGTH(word) > 3 ;ORDER BY RAND(); LIMIT 1', (error, results, fields) => {
-  if (error) throw error;
-  words[0] = results;
-});
-const startX = Math.floor(size / 2 - words[0].length / 2);
-const startY = Math.floor(size / 2);
-if (Math.random() < 0.5) {
-  placeWord(words[0], startX, startY, 1, 0);
-} else {
-  placeWord(words[0], startX, startY, 0, 1);
-}
+// connection.query('SELECT word FROM entries ;WHERE CHAR_LENGTH(word) > 3 ;ORDER BY RAND(); LIMIT 1', (error, results, fields) => {
+//   if (error) throw error;
+//   words[0] = results;
+// });
+// const startX = Math.floor(size / 2 - words[0].length / 2);
+// const startY = Math.floor(size / 2);
+// if (Math.random() < 0.5) {
+//   placeWord(words[0], startX, startY, 1, 0);
+// } else {
+//   placeWord(words[0], startX, startY, 0, 1);
+// }
